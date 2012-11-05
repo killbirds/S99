d2c9950bef718cb6170ddc5b910950a72b61202c
killbirds <kilbirds@tmax.co.kr>
2012-11-05 17:26:40 +0900

    ~ 12

--- src/test/scala/S99Test.scala
+++ src/test/scala/S99Test.scala
@@ -122,10 +122,22 @@ class S99Test extends FunSpec with ShouldMatchers {
 		}
 	}
 
+	def decode[T](list: List[(Int, T)]): List[T] = {
+		list.foldLeft(List[T]()) { case (result, (size, entry)) =>
+			result ::: (0 until size).map{index => entry}.toList
+		}
+	}
+
 	describe("11 to 20 problems") {
 		it("11. Modified run-length encoding.") {
 			info("Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N, E) terms.")
 			encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be (List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e)))
 		}
+
+		it("12. Decode a run-length encoded list.") {
+			info("Given a run-length code list generated as specified in problem P10, construct its uncompressed version.")
+			decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should be (List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+		}
+
 	}
 } 
